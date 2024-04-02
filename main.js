29f6740fa8bb526cd431befd7b5a285a95f20e82
Paul Yonggi Cho <100851513+paulyonggicho@users.noreply.github.com>
2024-04-01 23:28:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+function tocaSom(seletorAudio){
+ const elemento = document.querySelector(seletorAudio);
+ 
+
+    if(elemento != null && elemento.localName === 'audio' ){
+
+            elemento.play();
+
+    }
+    else{
+        console.log("Elemento não Encontrado ou Seletor Invalido");
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;  //template string
+
+    console.log(idAudio);
+
+    tecla.onclick = function() {
+      
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function (evento){
+
+       
+        if (evento.code === 'Space' || evento.code === 'Enter' ){
+            tecla.classList.add('ativa');
+        }
+
+    
+        
+    }
+
+        tecla.classList.remove('ativa');
+    //console.log(contador);
+
+}
+
